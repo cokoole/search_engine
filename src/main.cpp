5b830aba9e669aa57eaf52174ce6124660740439
cokoole <vc.vc9394@gmail.com>
2022-06-09 19:00:23 +0700

    Changes main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,18 @@
 #include "SearchServer.h"
 
 int main() {
-  ConverterJSON converter_json;
-  InvertedIndex inverted_index;
 
   try {
+    ConverterJSON converter_json("../config.json", "../requests.json", "../answers.json");
+    InvertedIndex inverted_index;
+
     auto docsPath = converter_json.GetTextDocuments();
     auto requests = converter_json.GetRequests();
 
     inverted_index.UpdateDocumentBase(docsPath);
     SearchServer search_server(inverted_index);
 
-    converter_json.putAnswers(search_server.search(requests, converter_json.GetResponsesLimit()));
+    converter_json.putAnswers(search_server.search(requests, converter_json.GetResponseLimit()));
   } catch (std::invalid_argument &x) {
     std::cout << x.what() << std::endl;
   }
